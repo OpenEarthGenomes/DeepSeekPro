c1807f4a2fae130d3cae82bc3cdf30830085159e
Fáber Sándor <sandorwow@gmail.com>
2026-06-23 15:24:16 +0200

    Update ExampleUnitTest.kt

--- test/java/com/deepseekpro/ExampleUnitTest.kt
+++ test/java/com/deepseekpro/ExampleUnitTest.kt
@@ -1,1 +1,11 @@
+package com.deepseekpro
 
+import org.junit.Test
+import org.junit.Assert.*
+
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
