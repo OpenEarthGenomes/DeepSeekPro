6f0a56e0e9fe44a9c0165c6540960857de88cb79
Fáber Sándor <sandorwow@gmail.com>
2026-06-23 14:20:38 +0200

    Update SearchResult.kt

--- app/src/main/java/com/deepseekpro/models/SearchResult.kt
+++ app/src/main/java/com/deepseekpro/models/SearchResult.kt
@@ -1,1 +1,12 @@
+package com.deepseekpro.models
 
+data class SearchResult(
+    val fileName: String,
+    val fileDate: String,
+    val messageIndex: Int,
+    val role: String,
+    val content: String,
+    val context: String,
+    val lineNumber: Int,
+    val matchCount: Int = 1
+)
